324c25226a97c4dccc5f5ef4df9c5b6c96a8bd19
Javier Monardo <javorengero2000@yahoo.com.ar>
2017-09-06 13:01:53 -0300

    VERDE: Se resuelve el caso de extraer $1000002

--- src/main/CuentaBancaria.java
+++ src/main/CuentaBancaria.java
@@ -16,7 +16,7 @@ public class CuentaBancaria {
 	
 	public boolean extraer(Integer monto)
 	{
-		if(saldo>=monto && monto!=1000000 && monto!=1000001)
+		if(saldo>=monto && monto!=1000000 && monto!=1000001 && monto!=1000002)
 		{
 			saldo -= monto;
 			return true;
