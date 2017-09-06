d01e475ae853aad3eb7fb849fa4b385079fe11cf
Javier Monardo <javorengero2000@yahoo.com.ar>
2017-09-06 12:22:42 -0300

    ROJO: Primera prueba.  Se agrega depositar.

--- Test/test/CuentaTest.java
+++ Test/test/CuentaTest.java
@@ -3,6 +3,16 @@ package test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import main.CuentaBancaria;
+
 public class CuentaTest {
+	
+	@Test
+	public void testDepositar100()
+	{
+		CuentaBancaria c1 = new CuentaBancaria();
+		c1.depositar();
+		Assert.assertEquals(100, c1.getSaldo());
+	}
 
 }
